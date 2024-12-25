d4a940ba356e48b6c19cc47b7c7f6900a1c42f8d
Tymkiv Oleksandr <t755631@gmail.com>
2024-12-25 17:51:33 +0200

    first commit

---
+++ SecurityModule/src/main/java/com/project/securitymodule/SecurityModuleApplication.java
@@ -0,0 +1,13 @@
+package com.project.securitymodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityModuleApplication.class, args);
+    }
+
+}
