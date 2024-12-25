1257c0d5a0cbb302e0d6b4d544cdb0c52bb2ee8c
Tymkiv Oleksandr <t755631@gmail.com>
2024-12-25 17:51:09 +0200

    first commit

---
+++ src/main/java/com/project/superduperproject/SuperDuperProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.superduperproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDuperProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperDuperProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/superduperproject/SuperDuperProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.superduperproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDuperProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
