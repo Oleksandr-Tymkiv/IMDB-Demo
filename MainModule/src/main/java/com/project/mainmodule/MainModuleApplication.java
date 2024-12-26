e2468bf6ceb6d5b84eb4ae44916174a47e200ed0
Tymkiv Oleksandr <t755631@gmail.com>
2024-12-26 17:02:02 +0200

    Implementation Spring Security

--- MainModule/src/main/java/com/project/mainmodule/MainModuleApplication.java
+++ MainModule/src/main/java/com/project/mainmodule/MainModuleApplication.java
@@ -2,10 +2,10 @@ package com.project.mainmodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({com.project.securitymodule.SecurityModuleApplication.class})
 public class MainModuleApplication {
 
     public static void main(String[] args) {
---
+++ SecurityModule/src/main/java/com/project/securitymodule/auth/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.project.securitymodule.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String name;
+    private LocalDate dateOfBirth;
+    private String email;
+    private String password;
+}
